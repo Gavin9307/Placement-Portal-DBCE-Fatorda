bc60bb9a04813ce16833469bf52858c81f2ad847
Gavin9307 <gavindacosta9307@gmail.com>
2024-07-16 16:48:55 +0530

    seperate header, head and footer

--- Students/notifications.php
+++ Students/notifications.php
@@ -2,41 +2,14 @@ php<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-    <script defer src="../FontAwesome/JS/all.js"></script>
+    <?php include './head.php' ?>
     <link rel="stylesheet" href="./css/notifications.css">
-    <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="./css/footer.css">
     <title>Notifications</title>
 </head>
 
 <body>
     <div id="wrapper">
-        <header>
-            <div class="header-container">
-                <div class="left-part">
-                    <img src="../Assets/dbce-logo.jpeg" alt="" class="logo">
-                    <h2>Placement Portal - Don Bosco College of Engineering</h2>
-                </div>
-                <ul class="right-part">
-                    <a href="./dashboard.php"><li>Dashboard</li></a>
-                    <li>Contact Us</li>
-                    <div class="notifications">
-                        <div class="dot">.</div><i class="fa-solid fa-bell"></i>
-                    </div>
-                    <li class="profile-container">
-                        <span>Gavin</span>
-                        <img src="../Assets/profile.jpg" alt="">
-                    </li>
-                </ul>
-            </div>
-        </header>
+        <?php include './header.php' ?>
 
         <div class="container">
             <?php include './sidebar.php' ?>
@@ -67,27 +40,7 @@ php<!DOCTYPE html>
             </div>
         </div>
 
-        <footer>
-            <div class="footer-container">
-                <div class="left-footer-part">
-                    <h4>Address</h4>
-                    <p>Don Bosco College of Engineering, Fatorda, Margao, Goa - 403602<br>Tel No.: (0832)
-                        2744111/2744112<br>E-mail : dbcefatorda@dbcegoa.ac.in</p>
-                </div>
-                <div class="right-footer-part">
-                    <h4>Follow us</h4>
-                    <div class="social-icons-list">
-                        <a href="https://www.linkedin.com/school/don-bosco-college-of-engineering-fatorda-goa/"><i
-                                class="ico fa-brands fa-square-facebook fa-3x"></i></a>
-                        <a href="https://www.instagram.com/dbce_fatorda/"><i
-                                class="ico fa-brands fa-square-instagram fa-3x"></i></a>
-                        <a
-                            href="https://www.facebook.com/p/Don-Bosco-College-of-EngineeringFatorda-Goa-100063749630574/"><i
-                                class="ico fa-brands fa-linkedin fa-3x"></i></a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include './footer.php' ?>
     </div>
 
 </body>

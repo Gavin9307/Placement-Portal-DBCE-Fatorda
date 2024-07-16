3e7e21e2343e8be3fb8e30291c302f00a83889c9
Syndroy <103412029+Syndroy@users.noreply.github.com>
2024-07-16 10:48:37 +0530

    changed html to php

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
                     <h2>Placement Portal - Don Bosco College of Engineering</h2>
                 </div>
                 <ul class="right-part">
-                    <a href="./sign-in.html"><li>Sign in</li></a>
-                    <a href="./sign-up.html"><li>Sign up</li></a>
-                    <a href="./student-support.html"><li>Contact Us</li></a>
+                    <a href="./sign-in.php"><li>Sign in</li></a>
+                    <a href="./sign-up.php"><li>Sign up</li></a>
+                    <a href="./student-support.php"><li>Contact Us</li></a>
                 </ul>
             </div>
         </header>
@@ -27,8 +27,8 @@
                 <div class="main-left-part">
                     <h3>Welcome to Don Bosco <br> College of Engineering <br> Placement <br> Portal</h3>
                     <div class="button-container">
-                        <a href="./sign-up.html"><button class="sign-up">Sign up</button></a>
-                        <a href="./sign-in.html"><button class="sign-in">Sign in</button></a>
+                        <a href="./sign-up.php"><button class="sign-up">Sign up</button></a>
+                        <a href="./sign-in.php"><button class="sign-in">Sign in</button></a>
                     </div>
                 </div>
                 <img src="./Assets/interview-image.jpg" alt="">

6713ca81e49fff5736a406a9dbb98a5bf7290add
Syndroy <103412029+Syndroy@users.noreply.github.com>
2024-07-17 11:45:12 +0530

    added css files and sidebar page

--- PlacementCoordinator/dashboard.php
+++ PlacementCoordinator/dashboard.php
@@ -1,11 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './head.php' ?>
+
+    <link rel="stylesheet" href="./css/dashboard.css">
     <title>Dashboard</title>
 </head>
+
 <body>
-    
+    <div id="wrapper">
+        <?php include './header.php' ?>
+
+        <div class="container">
+            <?php include './sidebar.php' ?>
+
+            <div class="main-container">
+                <h2 class="main-container-heading">Dashboard</h2>
+                <div class="sections">
+                    <h3>Performance and Metrics</h3>
+                    <div class="sub-sections performance">
+                        <div class="right1"><i class=" fa-solid fa-chevron-right fa-2x" style="color: #000000;"></i>
+                        </div>
+                        <p>Total Applications : 5</p>
+                        <p>Interviews Attended : 3</p>
+                        <p>Rejections : 3</p>
+                    </div>
+                </div>
+
+                <div class="sections">
+                    <h3>My Applications</h3>
+                    <div class="sub-sections companies">
+                        <div class="right1"><a href="./my-applications.php"><i class=" fa-solid fa-chevron-right fa-2x" style="color: #000000;"></i></a>
+                        </div>
+                        <div class="sub-table">
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>Associate Developer</p>
+                            </div>
+                            <hr>
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>Software Analyst</p>
+                            </div>
+                            <hr>
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>UI/UX Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sections">
+                    <h3>Job Opportunities</h3>
+                    <div class="sub-sections companies">
+                        <div class="right1"><i class=" fa-solid fa-chevron-right fa-2x" style="color: #000000;"></i>
+                        </div>
+                        <div class="sub-table">
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>Associate Developer</p>
+                            </div>
+                            <hr>
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>Software Analyst</p>
+                            </div>
+                            <hr>
+                            <div class="sub-table-row">
+                                <img src="../Assets/dbce-logo.jpeg" alt="">
+                                <p>UI/UX Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <?php include './footer.php' ?>
+    </div>
+
 </body>
+
 </html>
---
+++ PlacementCoordinator/sidebar.php
@@ -0,0 +1,18 @@
+<div class="side-bar-container">
+    <div class="fixed-container">
+        <div class="top-container">
+            <img src="../Assets/profile.jpg" alt="">
+            <p>Gavin Da Costa</p>
+            <p>2114018@dbcegoa.ac.in</p>
+            <a href=""><button>Update Profile</button></a>
+        </div>
+
+        <div class="bottom-container">
+            <a href="./performance-and-metrics.php"><button id="performance">Performance</button></a>
+            <a href="./my-applications.php"><button id="myapplications">My Application</button></a>
+            <a href="./job-opportunities.php"><button id="jobopportunities">Job Opportunities</button></a>
+            <a href="./companies.php"><button id="companies">Company Information</button></a>
+            <a href="./notifications.php"><button id="notifications">Notifications</button></a>
+        </div>
+    </div>
+</div>

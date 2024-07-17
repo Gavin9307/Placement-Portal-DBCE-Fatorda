c62c901919bdfe584bf6249de3eca1c3be08c50a
Syndroy <103412029+Syndroy@users.noreply.github.com>
2024-07-17 11:27:52 +0530

    tpo section

---
+++ PlacementCoordinator/header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="header-container">
+        <div class="left-part">
+            <img src="../Assets/dbce-logo.jpeg" alt="" class="logo">
+            <h2>Placement Portal - Don Bosco College of Engineering</h2>
+        </div>
+        <ul class="right-part">
+            <li>Contact Us</li>
+            <div class="notifications">
+                <div class="dot">.</div><i class="fa-solid fa-bell"></i>
+            </div>
+            <li class="profile-container">
+                <span>Gavin</span>
+                <img src="../Assets/profile.jpg" alt="">
+            </li>
+        </ul>
+    </div>
+</header>

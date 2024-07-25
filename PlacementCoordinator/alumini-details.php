32ee67620336a8392162e5221e325198dc9e0102
Stephen <stephenferns2003@gmail.com>
2024-07-25 16:13:02 +0530

    created php and css file for alumini-details

---
+++ PlacementCoordinator/alumini-details.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include './head.php' ?>
+    <link rel="stylesheet" href="./css/alumini-details.css">
+    <title>Notifications</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php include './header.php' ?>
+
+        <div class="container">
+            <?php include './sidebar.php' ?>
+
+            <div class="main-container">
+                <div class="main-container-header">
+                <h2 class="main-container-heading"><a href="./dashboard.html"><i class="fa-solid fa-arrow-left fa-lg" style="color: #000000;"></i></a>
+                    Alumini reports</h2>
+                       
+                </div>
+            <h3>Nimish Noronha</h3>
+                <div class="sections">
+                    <div class = "sec1">
+                         <p><strong>Department </strong>: MECH</p>
+                         <p><strong>Class </strong>:TE MECH</p>
+                         <p><strong>Contact  </strong>: 9483827383</p>
+                         <p><strong>Email </strong>: 2114018@dbcegoa.ac.in</p>
+                         <p><strong>Address </strong>: Hno. 336,Guidolim , Goa</p>
+                         <p><strong>Compus placement </strong>: Placed</p>
+                         <p><strong>Company </strong>: Google</p>
+                         <p><strong>Offered Salary </strong>: 3,00,000</p>
+                         <p><strong>Offer letter </strong>: letter.pdf</p>  
+
+                    </div>
+                    <div class= "sec2">   
+                    <img src="../Assets/profile.jpg" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <?php include './footer.php' ?>
+    </div>
+
+</body>
+
+</html>

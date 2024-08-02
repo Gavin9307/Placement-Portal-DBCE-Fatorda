30da8c87d3a581681975972e5d327b02384a967b
Pierson Fernandes <fernandespierson03@gmail.com>
2024-08-02 21:34:32 +0530

    created placement coordinator details page

--- PlacementCoordinator/Placement-Coordinator-Details.php
+++ PlacementCoordinator/Placement-Coordinator-Details.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include './head.php' ?>
-    <link rel="stylesheet" href="./css/Placement-Coordinator.css">
+    <link rel="stylesheet" href="./css/Placement-Coordinator-Details.css">
     <title>Placement Coordinators</title>
 </head>
 
@@ -18,44 +18,41 @@
                 <div class="main-container-header">
                 <h2 class="main-container-heading"><a href="./dashboard.html"><i class="fa-solid fa-arrow-left fa-lg" style="color: #000000;"></i></a>
                 Placement Coordinators</h2>    
-                <div class="button-container">
-                <a href="./notification-post.php"><button class="viewmore-button">Add Coordinator+</button></a>
-                </div>
                 </div>
                 <h2 class="main-container-heading"><a href="./dashboard.html"><i class="fa-solid fa-lg" style="color: #000000;"></i></a>
-                Placement Coordinators</h2> 
-
-            
-                    
-                 <div class="sections">
-                    <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Details</th>
-                    </tr>
-                    <tr>
-                        <td>Nimish</td>
-                        <td>MECH</td>
-                        <td><a href="">View more</a></td>
-                    </tr>
-                    <tr>
-                        <td>Syndroy</td>
-                        <td>COMP</td>
-                        <td><a href="">View more</a></td>
-                    </tr>
-                    <tr>
-                        <td>Bliss</td>
-                        <td>COMP</td>
-                        <td><a href="">View more</a></td>
-                    </tr>
-                    <tr>
-                        <td>Warren</td>
-                        <td>COMP</td>
-                        <td><a href="">View more</a></td>
-                    </tr>
-                </table>
-                
+                Nimish Noronha</h2> 
+                <div class="sections">
+                    <div class = "sec1">
+                    <form action="" method="post" enctype="multipart/form-data">
+                    <div class="form-adjust">
+                    <div class="inputbox">
+                                <label for="">First Name :</label>
+                                <input name="first_name" type="text" placeholder="Student">
+                    </div>
+                    <div class="inputbox">
+                                <label for="">Last Name :</label>
+                                <input name="last_name" type="text" placeholder="Last Name">
+                    </div>
+                    <div class="inputbox">
+                                <label for="">Department :</label>
+                                <input name="department" type="text" placeholder="Department">
+                    </div>
+                    <div class="inputbox">
+                                <label for="">Contact No :</label>
+                                <input name="contact_no" type="text" placeholder="Contact No">
+                    </div>
+                    <div class="inputbox">
+                                <label for="">Email :</label>
+                                <input name="email" type="text" placeholder="Email">
+                    </div>
+                    </div>
+                    </form>
+                    </div>
+                    <div class= "sec2">   
+                    <img src="../Assets/profile.jpg" alt="">
+                    <div><a href="#"><button class="add-button">Change Image</button></a></div>
+                    <div><a href="#"><button class="add-button">Update</button></a></div>
+                    </div>
                 </div>         
              
             </div>

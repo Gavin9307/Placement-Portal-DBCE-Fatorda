c7d67abaccb6c28b44ed5148ca2dfadcc5a54f79
Syndroy <103412029+Syndroy@users.noreply.github.com>
2024-08-03 21:55:08 +0530

    created analysis and report pages of company and students

--- PlacementCoordinator/analysis-and-report-yearly.php
+++ PlacementCoordinator/analysis-and-report-yearly.php
@@ -16,67 +16,65 @@
 
             <div class="main-container">
                 <h2 class="main-container-heading"><a href="./dashboard.html"><i class="fa-solid fa-arrow-left fa-lg" style="color: #000000;"></i></a>
-                    Analysis and Reports</h2>
+                    Analysis and Reports</h2>        
                 <h3>Yearly Placement Drive Reports</h3>
                 <div class="sections">
                     <div class="form-adjust">
-                        <form action="">
-                            <div class="datebox">
-                                <div>
-                                    <label for=""><strong>From:</strong></label>
-                                    <input type="date">
-                                </div>
-                                <div>
-                                    <label for=""><strong>To:</strong></label>
-                                    <input type="date">
-                                </div>
-                            </div>
+                    <form action="">
+                        <div class="datebox">
+                            <div>
+                                <label for=""><strong>From:</strong></label>
+                                <input type="date" >
+                            </div>   
+                            <div>
+                                <label for=""><strong>To:</strong></label>
+                                <input type="date" >
+                            </div> 
+                            </div>  
 
-                            <div class="departmentbox">
+                             <div class="departmentbox">
                                 <label for=""><strong>Department</strong></label>
-                                <div class="Checkbox">
-                                    <div>
-                                        <input type="checkbox">
-                                        <label for="">ECS</label>
-                                    </div>
-
-                                    <div><input type="checkbox">
-                                        <label for="">COMP</label>
-                                    </div>
-                                    <div><input type="checkbox">
-                                        <label for="">MECH</label>
-                                    </div>
-                                    <div><input type="checkbox">
-                                        <label for="">CIVIL</label>
-                                    </div>
+                            <div class="Checkbox"> 
+                                <div>
+                                    <input type="checkbox">  
+                                     <label for="">ECS</label> 
+                                </div>
+                                
+                                <div><input type="checkbox">  
+                                <label for="">COMP</label></div>
+                                <div><input type="checkbox">  
+                                <label for="">MECH</label></div>
+                                <div><input type="checkbox">  
+                                <label for="">CIVIL</label> </div> 
 
                                 </div>
 
-                            </div>
-                            <div class="getreportbutton">
-                                <a href=" "><button class="add-button">Get Report</button></a>
-                            </div>
-                        </form>
-                    </div>
+                             </div> 
+                             <div class="getreportbutton">
+                             <a href=" "><button class="add-button">Get Report</button></a>
+                            </div>       
+                    </form>                 
+                </div>
                 </div>
                 <div class="sections section-container">
 
-                    <div class="sections-1">
-                        <p><strong>Total Number of students</strong>: 100</p>
-                        <p><strong>Total Number of students Placed</strong>: 20</p>
-                        <iframe class="responsive-iframe" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQwj2XQEskPgLtqr0YpmTsA4eb36sbaUt16rtthRpDURHxbVRPVZVkr-icApfmjR0Lv0WiMdjeGzFEV/pubchart?oid=1931950964&amp;format=interactive"></iframe>
-                    </div>
-                    <div class="sections-1">
-                        <p><strong>Total Number of Companies</strong>: 20</p>
-                        <p><strong>Companies That hired</strong>: 8</p>
-                        <iframe class="responsive-iframe" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQwj2XQEskPgLtqr0YpmTsA4eb36sbaUt16rtthRpDURHxbVRPVZVkr-icApfmjR0Lv0WiMdjeGzFEV/pubchart?oid=1931950964&amp;format=interactive"></iframe>
-                    </div>
-
+                <div class="sections-1">
+                    <p><strong>Total Number of students</strong>: 100</p>
+                    <p><strong>Total Number of students Placed</strong>: 20</p>
+                    <iframe  class="responsive-iframe" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQwj2XQEskPgLtqr0YpmTsA4eb36sbaUt16rtthRpDURHxbVRPVZVkr-icApfmjR0Lv0WiMdjeGzFEV/pubchart?oid=1931950964&amp;format=interactive"></iframe>
+                </div>
+                <div class="sections-1">
+                    <p><strong>Total Number of Companies</strong>: 20</p>
+                    <p><strong>Companies That hired</strong>: 8</p>
+                    <iframe  class="responsive-iframe" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQwj2XQEskPgLtqr0YpmTsA4eb36sbaUt16rtthRpDURHxbVRPVZVkr-icApfmjR0Lv0WiMdjeGzFEV/pubchart?oid=1931950964&amp;format=interactive"></iframe>
+                </div>
+               
 
                 </div>
                 <h3><strong>Student Details</strong></h3>
 
                 <div class="sections">
+<<<<<<< Updated upstream
                     
                     <table>
                         <tr>
@@ -125,6 +123,56 @@
                             </div>
                         </div>
                     </div>
+=======
+                    <h3><strong>Student Details</strong></h3>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Company</th>
+                        <th>Offered Salary</th>
+                        <th>Joining Date</th>
+                        <th>Details</th>
+                    </tr>
+                    <tr>
+                        <td>Nimish</td>
+                        <td>Google</td>
+                        <td>30,000</td>
+                        <td>2/3/2024</td>
+                        <td><a href="">View more</a></td>
+                    </tr>
+                    <tr>
+                        <td>Patric</td>
+                        <td>Facebook</td>
+                        <td>20,000</td>
+                        <td>30/3/2024</td>
+                        <td><a href="">View more</a></td>
+                    </tr>
+                    <tr>
+                        <td>Gavin</td>
+                        <td>Google</td>
+                        <td>50,000</td>
+                        <td>12/11/2024</td>
+                        <td><a href="">View more</a></td>
+                    </tr>
+                    <tr>
+                        <td>Stephen</td>
+                        <td>Reliance</td>
+                        <td>40,000</td>
+                        <td>24/5/2024</td>
+                        <td><a href="">View more</a></td>
+                    </tr>
+                </table>
+                <div class="button-container-1">
+                <div class="dropdown">
+        <button class="viewmore-button">View More</button>
+        <div class="dropdown-content">
+            <a href="./notification-post.php">Option 1</a>
+            <a href="./another-link.php">Option 2</a>
+            <a href="./yet-another-link.php">Option 3</a>
+        </div>
+    </div>
+                </div>
+>>>>>>> Stashed changes
                 </div>
                 <h3>Other Reports:</h3>
                 <div class="button-container-2">

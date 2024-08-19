1115d23616347ebab4f2c1c95f81723ccc55b7d6
Gavin9307 <gavindacosta9307@gmail.com>
2024-08-19 07:20:13 +0530

    .

--- PlacementCoordinator/analysis-and-report.php
+++ PlacementCoordinator/analysis-and-report.php
@@ -17,82 +17,57 @@
             <div class="main-container">
                 <h2 class="main-container-heading"><a href="./dashboard.php"><i class="fa-solid fa-arrow-left fa-lg" style="color: #000000;"></i></a>
                     Analysis and Reports</h2>
-                <!-- <h3>Live Placement Drive Reports</h3> -->
-                <!-- <div class="sections">
-                    <div class="company-container">
-                        <div class="company-logo-container">
-                            <img src="../Assets/profile.jpg" alt="">
-                            <p>Google</p>
-                        </div>
-                        <p><strong>Due Date:</strong> 12/12/2003</p>
-                    </div>
-                    <p class="position"><strong>Position:</strong> Associate Developer</p>
-                    <p class="department"><strong>Departments</strong>: Computer</p>
-                    <a href="./job-live-listing-analysis.php"><button>View Report</button></a>
-                </div>
 
-                <div class="sections">
-                    <div class="company-container">
-                        <div class="company-logo-container">
-                            <img src="../Assets/profile.jpg" alt="">
-                            <p>Google</p>
-                        </div>
-                        <p><strong>Apply Date:</strong> 12/12/2003</p>
-                    </div>
-                    <p class="position"><strong>Position:</strong> Associate Developer</p>
-                    <p class="department"><strong>Departments</strong>: Computer</p>
-                    <a href="./my-applications-details.html"><button>View Report</button></a>
-                </div> -->
                 <h3>Yearly Placement Drive Reports</h3>
                 <div class="sections">
-                <form action="">
-                <div class="form-adjust">
-                    <div class="datebox">
-                        <div>
-                            <label for=""><strong>From:</strong></label>
-                            <input type="date">
+                    <form action="">
+                        <div class="form-adjust">
+                            <!-- <div class="datebox">
+                                <div>
+                                    <label for=""><strong>From:</strong> </label>
+                                    <input name="dateStart" type="date">
+                                </div>
+                                <div>
+                                    <label for=""><strong>To:</strong> </label>
+                                    <input name="dateEnd" type="date">
+                                </div>
+                            </div> -->
+                            <div class="batch-container">
+
+                                <label for=""><strong>Batch: </strong></label>
+                                <select name="d_batch_year" id="d_batch_year">
+                                <option value="" selected>Select Batch</option>
+                                <?php
+                                $currentYear = date('Y');
+                                for ($year = $currentYear + 4; $year >= 2016 + 4; $year--) {
+                                    echo '<option value="' . $year . '">' . $year . '</option>';
+                                }
+                                ?>
+                            </select>
+
+                            </div>
+                            <div class="departmentbox">
+                                <label for=""><strong>Department: </strong></label>
+                                <div class="Checkbox">
+                                <?php
+                                    global $conn;
+                                    $fetchDepartmentQuery = "SELECT Dept_name as dname FROM department;";
+                                    $fetchDepartment = $conn->prepare($fetchDepartmentQuery);
+                                    $fetchDepartment->execute();
+                                    $result = $fetchDepartment->get_result();
+
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo '<div>
+                                            <input name="departments[]" value="' . htmlspecialchars($row["dname"]) . '" type="checkbox">
+                                            <label for="">' . htmlspecialchars($row["dname"]) . '</label>
+                                        </div>';
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <a href="./analysis-and-report-yearly.php"><button class="add-button">Get Report</button></a>
                         </div>
-                        <div>
-                            <label for=""><strong>To:</strong></label>
-                            <input type="date">
-                        </div>
-                    </div>
-                    <div class="batch-container">
-
-                                <label for=""><strong>Batch:</strong></label>
-                                <select name="" id="">
-                                    <option value="">2025</option>
-                                    <option value="">2024</option>
-                                    <option value="">2023</option>
-                                    <option value="">2022</option>
-                                    <option value="">2021</option>
-                                </select>
-
-                            </div>
-                    <div class="departmentbox">
-                        <label for=""><strong>Department</strong></label>
-                        <div class="Checkbox">
-                            <div>
-                                <input type="checkbox">
-                                <label for="">ECS</label>
-                            </div>
-
-                            <div><input type="checkbox">
-                                <label for="">COMP</label>
-                            </div>
-                            <div><input type="checkbox">
-                                <label for="">MECH</label>
-                            </div>
-                            <div><input type="checkbox">
-                                <label for="">CIVIL</label>
-                            </div>
-
-
-                        </div>
-                    </div>
-                    <a href="./analysis-and-report-yearly.php"><button class="add-button">Get Report</button></a>
-                </div>
-                </form>
+                    </form>
 
                 </div>
                 <h3>Other Reports:</h3>
--- Students/header.php
+++ Students/header.php
@@ -5,11 +5,15 @@
             <h2>Placement Portal - Don Bosco College of Engineering</h2>
         </div>
         <ul class="right-part">
-            <a href="./dashboard.php"><li>Dashboard</li></a>
-            <a href="../student-support.php"><li>Contact Us</li></a>
+            <a href="./dashboard.php">
+                <li>Dashboard</li>
+            </a>
+            <a href="../student-support.php">
+                <li>Contact Us</li>
+            </a>
             <div class="notifications">
-             <a href="./notifications.php">
-                <?php 
+                <a href="./notifications.php">
+                    <?php
                     global $conn;
                     $fetchNotificationsQuery = "SELECT ND.Message AS message
                     FROM 
@@ -17,23 +21,34 @@
                     INNER JOIN studentnotifications AS SN ON ND.Notification_ID = SN.Notification_ID
                     WHERE 
                     SN.S_College_Email = ? AND ND.Notification_Due_Date >= CURRENT_DATE;";
-                
+
                     $fetchNotifications = $conn->prepare($fetchNotificationsQuery);
                     $fetchNotifications->bind_param("s", $_SESSION["user_email"]);
                     $fetchNotifications->execute();
                     $result = $fetchNotifications->get_result();
-                
+
                     if ($result->num_rows > 0) {
-                     echo '<div class="dot">.</div>';
+                        echo '<div class="dot">.</div>';
                     }
-                ?>
-                
-                <i class="fa-solid fa-bell"></i></a>  
+                    ?>
+
+                    <i class="fa-solid fa-bell"></i></a>
             </div>
-           <a href="./my-profile.php"> <li class="profile-container">
-                <span>Gavin</span>
-                <img src="../Assets/profile.jpg" alt="">
-            </li></a>
+            <a href="./my-profile.php">
+                <li class="profile-container">
+                    <?php
+                    global $conn;
+                    $fetchUserQuery = "SELECT S_Fname,S_Profile_pic from student where S_College_Email = ?";
+                    $fetchUser = $conn->prepare($fetchUserQuery);
+                    $fetchUser->bind_param("s", $_SESSION["user_email"]);
+                    $fetchUser->execute();
+                    $result = $fetchUser->get_result();
+                    $row = $result->fetch_assoc();
+                    echo '<span>' . $row["S_Fname"] . '</span>
+                        <img src="../Data/Students/Profile_Images/' . $row["S_Profile_pic"] . '" alt="">';
+                    ?>
+                </li>
+            </a>
         </ul>
     </div>
 </header>

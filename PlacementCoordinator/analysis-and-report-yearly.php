f2e21cd29ffd77fb5c4969865414f995b1740ff4
Gavin9307 <gavindacosta9307@gmail.com>
2024-08-30 12:42:15 +0530

    Update analysis-and-report-yearly.php

--- PlacementCoordinator/analysis-and-report-yearly.php
+++ PlacementCoordinator/analysis-and-report-yearly.php
@@ -13,118 +13,9 @@ if (!isset($_SESSION)) {
 
 // Assuming the email is stored in the session
 $student_email = $_SESSION['user_email'];
-// Initialize counts with default values
-$Eligible = ['count' => 0];
-$Interested = ['count' => 0];
-$Rejected = ['count' => 0];
-$Placed = ['count' => 0];
-
-// Base query for all students eligible
-$base_sql = "SELECT COUNT(DISTINCT ja.S_College_Email) as count
-             FROM jobapplication as ja 
-             INNER JOIN jobplacements as jp ON jp.J_id = ja.J_id
-             INNER JOIN student as s ON s.S_College_Email = ja.S_College_Email
-             INNER JOIN jobdepartments as jd ON jd.J_id = ja.J_id
-             INNER JOIN department as d ON jd.Dept_id = d.Dept_id
-             WHERE jp.J_Due_date < CURRENT_DATE";
-
-$base_company_sql = "SELECT COUNT(DISTINCT ja.S_College_Email) as count
-             FROM jobapplication as ja 
-             INNER JOIN jobplacements as jp ON jp.J_id = ja.J_id
-             INNER JOIN student as s ON s.S_College_Email = ja.S_College_Email
-             INNER JOIN jobdepartments as jd ON jd.J_id = ja.J_id
-             INNER JOIN department as d ON jd.Dept_id = d.Dept_id
-             WHERE jp.J_Due_date < CURRENT_DATE";
 
 $report_Query = "SELECT  CONCAT_WS(' ', s.S_Fname, s.S_Mname, s.S_Lname) AS `Name`,d.Dept_name AS `Department`,  com.C_Name AS `Company`,  IFNULL(j.J_Offered_salary, 0) AS `Offered Salary`, j.J_Due_date AS `Joining Date`, com.C_Location AS `Location`, s.S_Year_of_Admission + 4 AS `Batch` FROM student AS s INNER JOIN class AS c ON c.Class_id = s.S_Class_id INNER JOIN  department AS d ON d.Dept_id = c.Dept_id INNER JOIN jobapplication AS ja ON ja.S_College_Email = s.S_College_Email INNER JOIN jobplacements AS j ON j.J_id = ja.J_id INNER JOIN jobposting AS jp ON jp.J_id = j.J_id INNER JOIN company AS com ON com.C_id = jp.C_id WHERE ja.placed = 1 AND j.J_Due_date < CURRENT_DATE";
 
-
-if (isset($_POST["get-filter-report"])) {
-    // Apply filters if set
-    if (!empty($_POST['d_batch_year'])) {
-        $batch_year = (int)$_POST['d_batch_year'] - 4;
-        $base_sql .= " AND s.S_Year_of_Admission = '$batch_year'";
-        $report_Query .= " AND s.S_Year_of_Admission = '$batch_year'";
-    }
-
-    if (!empty($_POST['departments'])) {
-        $departments = $_POST['departments'];
-        $departmentList = implode("','", array_map('htmlspecialchars', $departments));
-        $base_sql .= " AND d.Dept_name IN ('$departmentList')";
-        $report_Query .= " AND d.Dept_name IN ('$departmentList')";
-    }
-
-    // Queries for different categories
-    $sqlEligible = $base_sql;  // Default query for eligible students
-    $sqlInterested = $base_sql . " AND ja.interest = 1";
-    $sqlRejected = $base_sql . " AND ja.placed = 0 AND ja.interest = 1";
-    $sqlPlaced = $base_sql . " AND ja.placed = 1 AND ja.interest = 1";
-
-    // Execute queries and fetch results
-    $resultEligible = mysqli_query($conn, $sqlEligible);
-    $resultInterested = mysqli_query($conn, $sqlInterested);
-    $resultRejected = mysqli_query($conn, $sqlRejected);
-    $resultPlaced = mysqli_query($conn, $sqlPlaced);
-
-
-
-    if ($resultEligible && mysqli_num_rows($resultEligible) > 0) {
-        $Eligible = mysqli_fetch_assoc($resultEligible);
-    }
-
-    if ($resultInterested && mysqli_num_rows($resultInterested) > 0) {
-        $Interested = mysqli_fetch_assoc($resultInterested);
-    }
-
-    if ($resultRejected && mysqli_num_rows($resultRejected) > 0) {
-        $Rejected = mysqli_fetch_assoc($resultRejected);
-        $was_not_passed_json = $Rejected['count'];
-    } else {
-        $was_not_passed_json = 0;
-    }
-
-    if ($resultPlaced && mysqli_num_rows($resultPlaced) > 0) {
-        $Placed = mysqli_fetch_assoc($resultPlaced);
-        $was_passed_json = $Placed['count'];
-    } else {
-        $was_passed_json = 0;
-    }
-} else {
-    $sqlEligible = $base_sql;
-    $sqlInterested = $base_sql . " AND ja.interest = 1";
-    $sqlRejected = $base_sql . " AND ja.placed = 0 AND ja.interest = 1";
-    $sqlPlaced = $base_sql . " AND ja.placed = 1 AND ja.interest = 1";
-
-
-    $resultEligible = mysqli_query($conn, $sqlEligible);
-    $resultInterested = mysqli_query($conn, $sqlInterested);
-    $resultRejected = mysqli_query($conn, $sqlRejected);
-    $resultPlaced = mysqli_query($conn, $sqlPlaced);
-
-
-
-    if ($resultEligible && mysqli_num_rows($resultEligible) > 0) {
-        $Eligible = mysqli_fetch_assoc($resultEligible);
-    }
-
-    if ($resultInterested && mysqli_num_rows($resultInterested) > 0) {
-        $Interested = mysqli_fetch_assoc($resultInterested);
-    }
-
-    if ($resultRejected && mysqli_num_rows($resultRejected) > 0) {
-        $Rejected = mysqli_fetch_assoc($resultRejected);
-        $was_not_passed_json = $Rejected['count'];
-    } else {
-        $was_not_passed_json = 0;
-    }
-
-    if ($resultPlaced && mysqli_num_rows($resultPlaced) > 0) {
-        $Placed = mysqli_fetch_assoc($resultPlaced);
-        $was_passed_json = $Placed['count'];
-    } else {
-        $was_passed_json = 0;
-    }
-}
 
 if (isset($_POST["get-report-students"])) {
     $sql = urldecode($_POST['query']);
@@ -140,8 +31,8 @@ if (isset($_POST["get-report-students"])) {
     }
 
     // Specify the spreadsheet ID and range
-    $spreadsheetId = '1tV0U_6Lxomb-tT6ruU9eX2veKjht5-0W84xSh9AvZvU';
-    $range = 'Sheet1!A5:Z'; // Adjust range as needed to cover all possible cells
+    $spreadsheetId = '1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0';
+    $range = 'students_details!B7:Z'; // Adjust range as needed to cover all possible cells
 
     // Clear the existing content in the range
     try {
@@ -168,7 +59,7 @@ if (isset($_POST["get-report-students"])) {
         exit();
     }
 
-    header("Location: https://docs.google.com/spreadsheets/d/1fGnnbnpsG2Ep1brwKAGLwqVPpWybbwuBBK9j8Sxuc64");
+    header("Location: https://docs.google.com/spreadsheets/d/1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0");
     exit();
 }
 
@@ -238,76 +129,11 @@ if (isset($_POST["get-report-students"])) {
                 <div class="sections section-container">
 
                     <div class="sections-1">
-                        <p><strong>Total Number of students who were Eligible</strong>: <?php echo $Eligible['count']; ?></p>
-                        <p><strong>Total Number of students who Applied</strong>: <?php echo $Interested['count']; ?></p>
-                        <p><strong>Total Number of students Placed</strong>: <?php echo $Placed['count']; ?></p>
-                        <p><strong>Total Number of students Not Placed</strong>: <?php echo $Rejected['count']; ?></p>
-                        <canvas id="myPieChart"></canvas>
-                        <script>
-                            const ctx = document.getElementById('myPieChart').getContext('2d');
-                            const myPieChart = new Chart(ctx, {
-                                type: 'pie',
-                                data: {
-                                    labels: ['Placed', 'Not Placed'],
-                                    datasets: [{
-                                        data: [<?php echo $was_passed_json; ?>, <?php echo $was_not_passed_json; ?>],
-                                        backgroundColor: ['#FF6384', '#36A2EB'],
-                                        hoverBackgroundColor: ['#FF6384', '#36A2EB']
-                                    }]
-                                },
-                                options: {
-                                    responsive: true,
-                                    plugins: {
-                                        legend: {
-                                            position: 'top',
-                                        },
-                                        tooltip: {
-                                            enabled: true,
-                                        }
-                                    },
-                                    animation: {
-                                        animateScale: true,
-                                        animateRotate: true
-                                    }
-                                }
-                            });
-                        </script>
+                        
                     </div>
 
                     <div class="sections-1">
-                        <p><strong>Total Number of Companies</strong>: 20</p>
-                        <p><strong>Companies That hired</strong>: 8</p>
-                        <canvas id="myPieChart1" style="width: 50%; height: 150px;"></canvas>
-                        <script>
-                            const cty = document.getElementById('myPieChart1').getContext('2d');
-                            const myPieChart1 = new Chart(cty, {
-                                type: 'pie',
-                                data: {
-                                    labels: ['Was Eligible', 'Was Not Eligible'],
-                                    datasets: [{
-                                        data: [<?php echo $was_eligible_json; ?>, <?php echo $was_not_eligible_json; ?>],
-                                        backgroundColor: ['#FF6384', '#36A2EB'],
-                                        hoverBackgroundColor: ['#FF6384', '#36A2EB']
-                                    }]
-                                },
-                                options: {
-                                    responsive: true,
-
-                                    plugins: {
-                                        legend: {
-                                            position: 'top',
-                                        },
-                                        tooltip: {
-                                            enabled: true,
-                                        }
-                                    },
-                                    animation: {
-                                        animateScale: true,
-                                        animateRotate: true
-                                    }
-                                }
-                            });
-                        </script>
+                        
                     </div>
 
 
@@ -364,11 +190,6 @@ if (isset($_POST["get-report-students"])) {
                             <input type="text" value="<?php echo urlencode($report_Query); ?>" name="query" hidden>
                             <button name="get-report-students" class="download-button">Get report</button>
                         </form>
-                            <div class="dropdown-content">
-                                <a href="https://docs.google.com/spreadsheets/d/1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0/export?format=csv&id=1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0">Download as CSV</a>
-                                <a href="https://docs.google.com/spreadsheets/d/1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0/export?format=pdf&id=1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0">Download as PDF</a>
-                                <a href="https://docs.google.com/spreadsheets/d/1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0/export?format=xlsx&id=1wS7cTnPvG7zB5z2of8AsV-jDNu_E0coXZXER_iIxzS0">Download as Excel</a>
-                            </div>
                         </div>
                     </div>
                 </div>
